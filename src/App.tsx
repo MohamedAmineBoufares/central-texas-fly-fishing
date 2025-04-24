8bc6dd04ff77ef1cfc3f66a8f6f1b13e37cf63cb
Mohamed Amine Boufares <med-amine-boufares@waialys>
2025-04-24 14:57:35 +0100

    fixed mobile responsivness

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import { useMobile } from "./hooks/use-mobile";
-
 import Header from "./components/header";
 import Hero from "./components/hero";
 import Footer from "./components/footer";
 import Features from "./components/feature";
 import Story from "./components/story";
+import { useIsMobile } from "./hooks/use-mobile";
 
 function App() {
-  const isMobile = useMobile("md");
+  const isMobile = useIsMobile();
+
+
+  if (isMobile === null) return null;
 
   if (isMobile) {
     return (
